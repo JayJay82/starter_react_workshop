61fc70089a7f371dafb193087813c942b7a7e198
Costa <ccavallo@enetworks.it>
2018-11-24 17:22:48 +0100

    Search Bar

---
+++ src/components/searchbar.component.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+export default class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { value : '' };
+    }
+    valueChanged = (event) => {
+        this.setState({ value : event.target.value});
+        console.log(event.target.value);
+    }
+    render() {
+        return(
+            <div className="search-bar">
+                <input type="text" value={this.state.value}
+                onChange = { this.valueChanged }
+                />
+            </div>
+        )
+    }
+}
