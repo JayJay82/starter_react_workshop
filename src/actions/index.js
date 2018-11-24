bb27e942dab97d56fe58f9006db25500d2119e41
Costa <ccavallo@enetworks.it>
2018-11-24 19:14:16 +0100

    Featch Action Refactor + axios + redux promise

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,14 @@
+import axios from 'axios';
+const search_url = "https://www.googleapis.com/youtube/v3/search?key=AIzaSyDYY7s68X-pS4ouLUvEKNP1mzKZxOe3MoQ&part=snippet&order=relevance&maxResults=10&q=";
+
 
 export const FETCH_VIDEOS = 'FETCH_VIDEOS';
 export function fetchVideos(term) {
-    console.log(term);
+    const url = search_url + term;
+    const request = axios.get(url);
+    console.log(request);
     return {
         type : FETCH_VIDEOS,
-        payload : {}
+        payload : request
     };
 }
